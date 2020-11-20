45ebeeed6f7d69d180cfa853f81c82cf348b1596
LaurenStanton <62582002+LaurenStanton@users.noreply.github.com>
2020-11-20 12:19:29 -0500

    Update Raccoon_Skunk.py

--- Raccoon_Skunk.py
+++ Raccoon_Skunk.py
@@ -1,7 +1,7 @@
 # Puzzle box control system
 __version__="v6 02-02-2018"
 __author__="E. Bridge and J. Huizinga"
-#Licensed in the public domain#
+#Licensed under the MIT License#
 
 # Constants
 ID = "ANIMALXXXX"
